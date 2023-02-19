f16504fb9cbfa28ad2b7a47088757eb6bdd2bd69
Isabel Blanco Martínez <49027767+isabelblanco47@users.noreply.github.com>
2023-02-19 13:38:17 +0100

    json ya va

--- TFG/src/app/planificadores/planificadores-list/planificadores-list.component.ts
+++ TFG/src/app/planificadores/planificadores-list/planificadores-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatCardTitleGroup } from '@angular/material/card';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,7 +11,12 @@ export class PlanificadoresListComponent {
 
   numTareas = 0
   start_time = 0;
+  type = 0;
+  duration = 0;
+  priority = 0;
   archivo = null;
+  contador = 1
+  labelInfo = ""
 
   constructor(private router: Router) {
 
@@ -18,34 +24,54 @@ export class PlanificadoresListComponent {
 
   ngOnInit() {
     console.warn("NGONINT")
-    this.generarJSON()
+    this.labelInfo = "Información de la Tarea " + this.contador;
+    
+    
   }
 
-  elementos: any = []
-  solicitarNumeroTareas() {
-    console.warn("ENTRO")
-    const element = (document.getElementById("numTareas") as HTMLInputElement).value
-    if (element != null) {
-      this.numTareas = parseInt(element)
-      this.elementos = Array(this.numTareas).fill(0);
-    }
-    this.generarJSON()
-  }
-  emptyList: any[] = []
-  /**   JSONdatos = {
-      "tasks": this.emptyList
-    }*/
+  
   JSONdatos = { task: [{}] }
   generarJSON() {
+    let tarea = "T" + this.contador
+    let temp = {
+      "command": tarea,
+      "start_time": this.start_time,
+      "priority": this.priority,
+      "behaviour": this.behaviourDict,
+    }
+    this.JSONdatos.task.push(temp)
+    //delete this.JSONdatos.task[0] // Se elemina el primer campo del diccionario por 
+    console.log("CAMPOS JSON PRUEBA 2", this.JSONdatos)
+  }
 
-    for (let i = 1; i <= this.numTareas; i++) {
-      let tarea = "T" + i
-      let temp = { "command": tarea }
-      this.JSONdatos.task.push(temp)
+  almacenarDatosTask() {
+    const start_time = (document.getElementById("startTime") as HTMLInputElement).value
+    const priority = (document.getElementById("priority") as HTMLInputElement).value
+    // se parsean a integer los datos start time y priority recibidos por input
+    if (start_time != null && priority != null) {
+      this.start_time = parseInt(start_time)
+      this.priority = parseInt(priority)
+      this.generarJSON()
+    }
+    this.contador += 1
+    this.labelInfo = "Información de la Tarea " + this.contador;
+    (document.getElementById("startTime") as HTMLInputElement).value = "";
+    (document.getElementById("priority") as HTMLInputElement).value = "";
+  }
+
+  behaviourDict: any[] = []
+  almacenarDatosBehaviour() {
+    const type = (document.getElementById("type") as HTMLInputElement).value
+    const duration = (document.getElementById("duration") as HTMLInputElement).value
+    if (type != null && duration != null) {
+      this.type = parseInt(type)
+      this.duration = parseInt(duration)
+      let temp = { "type": this.type, "duration": this.duration }
+      this.behaviourDict.push(temp)
 
     }
-    delete this.JSONdatos.task[0] //se elimina el primer elemento del diccionario ya que es el clave-valor
-    console.log(this.JSONdatos, "JSON::::")
-    console.log("LONGITUD", Object.keys(this.JSONdatos.task).length)
+    (document.getElementById("type") as HTMLInputElement).value = "";
+    (document.getElementById("duration") as HTMLInputElement).value = "";
+    console.log("BEHAVIOUR", this.behaviourDict)
   }
 }
